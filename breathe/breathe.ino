bc49ee1f410234f2b3ba105b8813652f5ac0d839
Bill Waggoner <ctgreybeard@gmail.com>
2016-05-13 14:59:49 -0400

    Tightened the code further. Only compute the pulse width (level) once a millisecond max.

--- breathe/breathe.ino
+++ breathe/breathe.ino
@@ -9,44 +9,48 @@ void setup() {
   // put your setup code here, to run once:
   pinMode(PIN, OUTPUT);
   digitalWrite(PIN, LOW);
-//  Serial.begin(57600);
-//  Serial.print("LOW is ");Serial.println(LOW);
-//  Serial.print("HIGH is ");Serial.println(HIGH);
-//  Serial.print("true is ");Serial.println(true);
-//  Serial.print("false is ");Serial.println(false);
 }
 
-void breathe() {
+void breathe(uint16_t m) {
   static uint8_t count = 0;
-  static uint8_t last_pulse = 127;
+  static boolean last_pulse = false;
+  static uint16_t last_m = 65535;
+  static uint8_t level = 0;
   uint8_t cycle;
-  uint8_t level;
-  uint8_t pulse;
-  
-  cycle = beat8(CPM); // Sawtooth freq = CPM
-//  level = cycle;
-//  level = ease8InOutCubic(cycle); // 2.54ms / cycle (9.9µs / count)
-//  level = ease8InOutApprox(cycle); // 2.34ms / cycle (9.1µs / count)
-//  level = cubicwave8(cycle); // 2.62ms / cycle (10.2µs / count)?
-  level = quadwave8(cycle); // 2.50ms / cycle (9.8µs / count)
-//  level = triwave8(cycle); // 2.26ms / cycle (8.8µs / count)
+  boolean pulse;
 
-//  level = beat8(CPM); // Takes 2.20ms / cycle (8.5µs / count)
+  // Generating the level once every millisecond reduces quadwave8 to 1.13ms / cycle
+  if (m != last_m) {  // Check the cycle every millisecond only
+    last_m = m;
 
-//  level = beatsin8(CPM, DIM, BRT);  // 2.82ms / cycle (11.0µs / count)
+    cycle = beat8(CPM); // Sawtooth freq = CPM
 
-// Generate the actual pulse
-  pulse = squarewave8(count, level);
-  
-  if (pulse != last_pulse) {  // Checking pulse saves! sin:2.82ms / cycle (11.0µs), saw:2.16ms (8.4µs)
-    digitalWrite(PIN, pulse == 255 ? HIGH : LOW);
-    
-    last_pulse = pulse;
+    // Now convert the sawtooth into a smoother periodic waveform
+    //  level = ease8InOutCubic(cycle);  // 2.54ms / cycle (9.9µs / count)
+    //  level = ease8InOutApprox(cycle); // 2.34ms / cycle (9.1µs / count)
+    //  level = cubicwave8(cycle);       // 2.62ms / cycle (10.2µs / count)?
+    level = quadwave8(cycle);            // 2.50ms / cycle (9.8µs / count)
+    //  level = triwave8(cycle);         // 2.26ms / cycle (8.8µs / count)
+
+    // or we could generate the periodic waveform directly
+    //  level = beat8(CPM);              // Takes 2.20ms / cycle (8.5µs / count)
+
+    //  level = beatsin8(CPM, DIM, BRT); // 2.82ms / cycle (11.0µs / count)
   }
   
-  count += 1;
+  // Generate the actual pulse
+  pulse = count <= level;
+
+  count += 1; // Increment for the next time through
+
+  if (pulse != last_pulse) {  // Checking pulse saves! sin:2.82ms / cycle (11.0µs), saw:2.16ms (8.4µs)
+    digitalWrite(PIN, pulse);
+
+    last_pulse = pulse;
+  }
 }
 
 void loop() {
-  breathe();
+  uint16_t m = millis();
+  breathe(m);
 }
